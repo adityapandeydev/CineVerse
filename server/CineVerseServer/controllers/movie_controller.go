fd0e6eb37f4467c95f41fcf6fcb65cbc8546955b
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-11-12 23:12:10 +0530

    db connected

--- server/CineVerseServer/controllers/movie_controller.go
+++ server/CineVerseServer/controllers/movie_controller.go
@@ -1,11 +1,68 @@
 package controllers
 
 import (
+	"context"
+	"time"
+	"net/http"
+
+	"github.com/adityapandeydev/CineVerse/Server/CineVerseServer/database"
+	"github.com/adityapandeydev/CineVerse/Server/CineVerseServer/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	//"github.com/go-playground/validator/v10"
 )
 
-func GetMovies() gin.HandlerFunc {
+//var validate = validator.New()
+
+func GetMovies(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"message":"List of movies"})
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		var movieCollection *mongo.Collection = database.OpenCollection("movies", client)
+
+		cursor, err := movieCollection.Find(ctx, bson.D{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies."})
+		}
+		defer cursor.Close(ctx)
+
+		var movies []models.Movie
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies."})
+			return
+		}
+
+		c.JSON(http.StatusOK, movies)
+	}
+}
+
+func GetMovie(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var movieCollection *mongo.Collection = database.OpenCollection("movies", client)
+
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.D{{Key: "imdb_id", Value: movieID}}).Decode(&movie)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movie)
+
 	}
 }
--- server/CineVerseServer/main.go
+++ server/CineVerseServer/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	controller "github.com/adityapandeydev/CineVerse/Server/CineVerseServer/controllers"
+	"github.com/adityapandeydev/CineVerse/Server/CineVerseServer/database"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func main() {
@@ -12,7 +14,8 @@ func main() {
 		c.String(200, "Hello, CineVerse!")
 	})
 
-	router.GET("/movies", controller.GetMovies())
+	var client *mongo.Client = database.Connect()
+	router.GET("/movies", controller.GetMovies(client))
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
