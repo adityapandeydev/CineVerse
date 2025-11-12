7dd3a63fbf5d12853433fa1a62a7f64cb934e0b2
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-11-12 21:14:27 +0530

    basic server

--- server/CineVerseServer/main.go
+++ server/CineVerseServer/main.go
@@ -1,6 +1,17 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello from go!")
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, CineVerse!")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
 }
